f05b8d5106cb776bc5dd79b3f6ea40990009122d
azer775 <80197483+azer775@users.noreply.github.com>
2025-09-17 18:38:05 +0100

    Enable Eureka client and externalize configs for services
    Uncommented and enabled Eureka client dependencies for MedicalReport and analyse services. Updated application.properties to use config server for both services and moved database and JPA settings to config-server property files. Added records-service configuration to config-server and switched analyse-service to PostgreSQL. Also enabled Eureka registration in Ai-service FastAPI app.

--- Services/Ai-service/app/main.py
+++ Services/Ai-service/app/main.py
@@ -7,14 +7,14 @@ import os
 
 load_dotenv()
 app = FastAPI()
-""" @app.on_event("startup")
+@app.on_event("startup")
 async def startup_event():
     await eureka_client.init_async(
         eureka_server="http://localhost:8761/eureka",
         app_name="fastapi-service",
         instance_port=8000,
         instance_host="localhost"
-    ) """
+    ) 
 
 
 app.include_router(Extraction.router, prefix="/pdf", tags=["PDF"])
